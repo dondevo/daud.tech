b1e6371ef4b5a3efbdc3409d1300b7eb56755971
dondevo <daudomar10@gmail.com>
2018-12-17 19:19:31 -0500

    styling background

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ParticleComponent from './ParticleComponent';
 import './App.css';
 
+// eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          textAlign: 'center'
+        }}
+      >
+        <ParticleComponent />
+        <div className="App-header">
+          I am Daud!
+          <h1>Full Stack Developer || Student</h1>
+        </div>
       </div>
     );
   }
