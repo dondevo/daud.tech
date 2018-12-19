1eef820e2a0a6b34b3dbd4b2f09eaf59d5e9473e
dondevo <daudomar10@gmail.com>
2018-12-19 16:40:51 -0500

    Sticky header + style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
 import ParticleComponent from './ParticleComponent';
-import './App.css';
+import MyHeader from './components/Nav/Nav';
+import AboutPage from './components/Pages/AboutPage/About';
+import LandingPage from './components/Pages/LandingPage/LandingPage';
 
+const particleStyle = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100%',
+  textAlign: 'center'
+};
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
     return (
-      <div
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          textAlign: 'center'
-        }}
-      >
+      <div className="parent" style={particleStyle}>
+        <MyHeader />
         <ParticleComponent />
-        <div className="App-header">
-          I am Daud!
-          <h1>Full Stack Developer || Student</h1>
-        </div>
+        <LandingPage />
+        <AboutPage />
+        <h1>I am Daud!</h1>
+        <p>Full Stack Developer || Student</p>
       </div>
     );
   }
