d285b852fb2591921ed3bd971f79a3a45d101c8e
dondevo <daudomar10@gmail.com>
2018-12-19 16:41:45 -0500

    landing page

---
+++ src/components/Pages/LandingPage/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './LandingPage.scss';
+
+const LandingPage = () => (
+  <div className="landing-page">
+    <main>
+      <div className="intro-wrapper">
+        <div className="intro-name">Hello, I'm Daud!</div>
+        <div className="tagline">Full Stack Dev | Open-Source Enthusiast | Blogger | Ninja</div>
+      </div>
+    </main>
+  </div>
+);
+
+export default LandingPage;
