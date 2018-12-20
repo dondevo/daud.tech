bfa7da7d2ceefe255c23b17bfe1e2e6b58ebcb96
dondevo <daudomar10@gmail.com>
2018-12-20 17:23:47 -0500

    Update LandingPage

--- src/components/Pages/LandingPage/LandingPage.js
+++ src/components/Pages/LandingPage/LandingPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './LandingPage.scss';
+import Icons from '../../Icons/Icons';
+import ScrollToNext from '../../Scroll/ScrollToNext';
 
 const LandingPage = () => (
   <div className="landing-page">
@@ -8,6 +10,8 @@ const LandingPage = () => (
         <div className="intro-name">Hello, I'm Daud!</div>
         <div className="tagline">Full Stack Dev | Open-Source Enthusiast | Blogger | Ninja</div>
       </div>
+      <Icons />
+      <ScrollToNext pageSelector=".about-page" />
     </main>
   </div>
 );
