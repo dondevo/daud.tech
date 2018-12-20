f6f94a0c01daf40dd5541726b9e213d8651aea89
dondevo <daudomar10@gmail.com>
2018-12-20 17:24:57 -0500

    navbar update

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,29 +3,19 @@ import StickyHeader from 'react-sticky-header';
 import './Nav.scss';
 
 const MyHeader = () => (
-  <nav>
-    <StickyHeader
-      // This is the sticky part of the header.
-        header={(
-<div className="nav">
-      {' '}
-            <style jsx="true">
-              {`
-              .menu__item:hover {
-                border-bottom: 2px solid;
-              }
-            `}
-          </style>
-          <ul className="menu">
-            <li className="menu__item">Hello</li>
-            <li className="menu__item">Projects</li>
-      <li className="menu__item">Blog</li>
-  </ul>
-        </div>
-)}
-        headerOnly
-    />
-  </nav>
+  <StickyHeader
+    header={(
+<nav>
+  <ul className="nav">
+                <li className="menu__item" href="#">
+            Hello
+    </li>
+          <li className="menu__item">Projects</li>
+                <li className="menu__item">Blog</li>
+              </ul>
+              </nav>
+)    }
+  />
 );
 
 export default MyHeader;
